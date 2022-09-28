89ab526a02070674085b1149ca9c8dc425b22f5e
Dharmil Shah <dharmilshah476@gmail.com>
2022-09-28 14:26:21 +0530

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
-      <h1 className="text-3xl text-center font-bold underline">
-      Hello world!
-    </h1>   
+      <Header/>
     </div>
   );
 }
