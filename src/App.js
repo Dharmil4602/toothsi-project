85c06c0c2aae3f760a55201308effd79d403cdc3
Dharmil Shah <dharmilshah476@gmail.com>
2022-09-28 13:40:05 +0530

    Added tailwind css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <h1 className="text-3xl text-center font-bold underline">
+      Hello world!
+    </h1>   
     </div>
   );
 }
