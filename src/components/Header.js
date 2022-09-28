7353bd38158bbe346dc94d5b683188d9aede27a2
Dharmil Shah <dharmilshah476@gmail.com>
2022-09-28 15:42:16 +0530

    Added products

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
 
 function Header() {
-  return (
-    <div>Header</div>
-  )
+    return (
+      <div className="flex mx-auto p-3 bg-gray-300 justify-center w-fit">
+        <div className="mr-10 text-2xl">
+          <h1>Image</h1>
+        </div>
+        <div className="mx-10 mr-60 text-2xl">
+          <h1>Name</h1>
+        </div>
+        <div className="mx-2 mr-20 text-2xl">
+          <h1>Color</h1>
+        </div>
+        <div className="mx-2 mr-20 text-2xl">
+          <h1>Stock</h1>
+        </div>
+        <div className="mx-2 mr-20 text-2xl">
+          <h1>Price</h1>
+        </div>
+        <div className="mx-2 mr-20 text-2xl">
+          <h1>Buy</h1>
+        </div>
+      </div>
+    );
 }
 
-export default Header
+export default Header;
