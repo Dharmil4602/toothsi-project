44bfaa758cb9b42401df3411a44f18210eb89a0f
Dharmil Shah <dharmilshah476@gmail.com>
2022-09-30 16:45:41 +0530

    Added increment decrement logo

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -12,24 +12,7 @@ function Cart() {
       <p>
         You Have <span>4</span> Items In Cart
       </p>
-      <Table>
-        <thead>
-          <tr className="text-center">
-            <th>Id</th>
-            <th>Image</th>
-            <th>Name</th>
-            <th>Color</th>
-            <th>Stock</th>
-            <th>Price</th>
-            <th>Buy</th>
-          </tr>
-        </thead>
-        {
-          item.map((currentItem) => {
-            return <Item />
-          })
-        }
-      </Table>
+        <Item />
     </div>
   );
 }
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,21 +1,58 @@
-import React from 'react'
+import React from "react";
+import { Table } from "react-bootstrap";
+import products from "../Products";
 
 function Item() {
+    const handleIncrement = (item) => {
+        console.log("Increment");
+    };
+
+
+    const handleDecrement = (item) => {
+        console.log("Decrement");
+    };
   return (
     <>
-      <div className="cart-items">
-        <div className="cart-items-container">
-          <div className="items-info">
-            <div className="product-image"></div>
-            <div className="product-title"></div>
-            <div className="product-price"></div>
-            <div className="product-quantity"></div>
-            <div className="product-subtotal"></div>
-          </div>
-        </div>
-      </div>
+      <Table>
+        <thead>
+          <tr className="text-center">
+            <th>Image</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Subtotal</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((item, i) => (
+            <tr key={i} className="font-bold">
+              <td className="flex justify-center">
+                <img className="h-20 w-20" src={item.image} alt="product" />
+              </td>
+              <td className="text-center">{item.name}</td>
+              <td className="text-center">${item.price}</td>
+              <td className="quantity text-center">
+                <div className="">
+                  <button>
+                    <i className="uil uil-minus"></i>
+                  </button>
+                  <input className="w-4 mx-2" type="text" value=""/>
+                  <button onClick={handleIncrement}>
+                    <i className="uil uil-plus"></i>
+                  </button>
+                </div>
+              </td>
+              <td className="subTotal text-center">Subtotal</td>
+              <td className="delete text-center">
+                <i className="fa fa-trash" aria-hidden="true"></i>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </>
   );
 }
 
-export default Item
+export default Item;
